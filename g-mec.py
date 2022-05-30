93a0cf244ff4ba39192ba45dffa85ddbdc0f381b
anonems <93661372+anonems@users.noreply.github.com>
2022-05-30 22:39:19 +0200

    ok

--- g-mec.py
+++ g-mec.py
@@ -23,9 +23,7 @@ docu=[
 ]
 administration = [
     Node("Frais de scolarité ! Que souhaite tu savoir?","frais",[Node("Les frais de scolarités pour cette années s'élèvent à 7 500€","montant",[]),Node("Les modalitées de paiment sont indiquées sur t'on contrat d'inscription, si tu souhaite plus d'information je t'invite à te raprocher de la compta : compta@hetic.com","modalité",[]),]),
-    Node("De quel document à tu besoin ?","document",docu),
-    Node("envoi un mp a Brontis (@ErwinTheCat#8778)","",[]),
-    Node("https://assets.jobteaser.com/upload_file/uploads/Convention_de_Stage_2022_-_fran%C3%A7ais.3417430645.pdf","convention",[])
+    Node("De quel document à tu besoin ?","document",docu)
 ]
 stage = [
     Node("4 juillet ","date",[]),
@@ -42,21 +40,84 @@ alternance = [
     Node("Bravo ! envoi toute suite un mp à Brontis (@ErwinTheCat#8778) pour lui annoncer la bonne nouvelle, il te dira comment s'y prendre","trouvé",[]),
     docu[0]
 ]
+programation = [
+    Node("DISCORD ! Voici une serie de petit tuto pour bien débuter avec les bots discord ","discord",[]),
+    Node("BOUCLES ! Voici un tuto qui explique bien le fonctionnement des boucles ","boucle",[]),
+    Node("FONCTIONS ! Voici un tuto pour mieux comprendre les fonctions en python ","fonction",[]),
+    Node("CLASSES ! Voici un tuto pour mieux comprendre le fonctionnement des classes","classe",[]),
+    Node("DICTIONNAIRES ! Voici un tuto pour mieux comprendre le fonctionnement des dictionnaires","dictionnaire",[]),
+    Node("ARBRE ! Voici un tuto pour mieux comprendre le fonctionnement des arbres","arbre",[])
+]
+php = [
+    Node("BOUCLES ! Voici un tuto qui explique bien le fonctionnement des boucles ","boucle",[]),
+    Node("FONCTIONS ! Voici un tuto pour mieux comprendre les fonctions en php ","fonction",[]),
+    Node("Variables ! Voici comment s'y prendre avec les variables en php","variable",[]),
+    Node("Session ! Voici comment s'y prendre avec les session en php","session",[])
+]
+sql = [
+    Node("PDO ! Voici  comment fonctionne la fonction PDO ","pdo",[]),
+    Node("Insertion d'un element dans la base de donnée ! Voici comment s'y prendre ","insert",[]),
+    Node("Selectionner un element dans la base de donnée ! Voici comment s'y prendre ","select",[]),
+    Node("Metre a jour la base de donnée ! Voici comment s'y prendre ","update",[]),
+    Node("Suprimer un element de la base donnée ! Voici comment s'y prendre ","delete",[])
+]
 back = [
-    Node("php ! Quel language ?","back",[]),
-
+    Node("PHP ! qu'est-ce qui vous pose problème plus précisément ?","php",php),
+    Node("PYTHON ! qu'est-ce qui vous pose problème plus précisément ?","python",programation),
+    Node("SQL ! qu'est ce qui vous pose problème plus précisément ?","sql",sql)
+]
+html = [
+    Node("LES BALISES ! Voici un tuto qui explique bien le fonctionnement des balises html ","balises",[]),
+    Node("LES HEADERS ! Voici un tuto pour mieux comprendre le fonctionnement du header ","header",[]),
+    Node("LE BODY ! Voici un tuto pour mieux comprendre le fonctionnement du body de la page","body",[]),
+    Node("LES FOOTERS ! Voici un tuto pour mieux comprendre le fonctionnement du footer","footer",[])
+]
+css = [
+    Node("Rendre son site responsive ! Si t'a pas capté bien capté le cours voici un petit tuto qui le resume","responsive",[]),
+    Node("GRID ! Pas encore famillier avec cette propriete ? Regarde cette vidéo elle va surement t'aider ","grid",[]),
+    Node("Position ! Pas encore famillier avec cette propriete ? Regarde cette vidéo elle va surement t'aider","position",[]),
+    Node("Le style du texte ! Tu veut savoir comment bien mairiser le style de t'es textes  ? vient voir par la :","text",[])
+]
+javascript = [
+    Node("BOUCLES ! Voici un tuto qui explique bien le fonctionnement des boucles ","boucle",[]),
+    Node("FONCTIONS ! Voici un tuto pour mieux comprendre les fonctions en js ","fonction",[]),
+    Node("Variables ! Voici comment s'y prendre avec les variables en js","variable",[])
+]
+front = [
+    Node("HTML ! qu'est-ce qui vous pose problème plus précisément ?","html",html),
+    Node("CSS ! qu'est-ce qui vous pose problème plus précisément ?","css",css),
+    Node("JAVASCRIPT ! qu'est ce qui vous pose problème plus précisément ?","javascript",javascript)
+]
+scrum = [
+    Node("Le product owner","owner",[]),
+    Node("le scrum master","master",[]),
+    Node("Lz scrum team","team",[]),
+    Node("Le sprint","sprint",[])
+]
+techno = [
+    Node("Le reseau","reseau",[]),
+    Node("l'ia","ia",[]),
+    Node("Les email","email",[])
+]
+graphism=[
+    Node("Indesign","indesign",[]),
+    Node("Photoshop","photoshop",[]),
+    Node("Illustrator","illustrator",[])
+]
+design=[
+    Node("UI","ui",[]),
+    Node("UX","ux",[]),
+    Node("FIGMA","figma",[])
 ]
 cours = [
     Node("Back-end ! Quel language ?","back",back),
-    Node("Front-end ! Quel language ?","front",[]),
-    Node("Programation ! Quel language ?","programation",[]),
-    Node("Design ! Tu veut en savoir plus sur le logiciel FIGMA ou la théorie","design",[]),
-    Node("Outils graphique ! quel logiciel (indisign, photoshop, illustrator)","graphique",[]),
-    Node("Shell Unix ! De quoi à-tu besoin (un tuto , un cours)","shell",[]),
-    Node("Prise de parol ! De quoi à-tu besoin (un tuto , un cours)","prise de parole",[]),
-    Node("SCRUM ! De quoi à-tu besoin (un tuto , un cours)","scrum",[]),
-    Node("SGBDR ! De quoi à-tu besoin (un tuto , un cours)","SGBDR",[]),
-    Node("Technologie web ! De quoi à-tu besoin (une vidéo , un cours)","techno",[])
+    Node("Front-end ! Quel language ?","front",front),
+    Node("Programation ! En première année tu aborde que python, dit qu'est ce qui ne va pas avec ce language ?","programation",programation),
+    Node("Design ! Tu veut en savoir plus sur le logiciel FIGMA ou la théorie ui/ux","design",design),
+    Node("Outils graphique ! quel logiciel (indesign, photoshop, illustrator)","graphique",graphism),
+    Node("SCRUM ! Quel rôle t'interesse ?","scrum",scrum),
+    Node("SGBDR ! Que veut tu savoir en sql ?","SGBDR",sql),
+    Node("Technologie web ! De quoi à-tu besoin (une vidéo , un cours)","techno",techno)
 ]
 first_list = [
     Node("Un cours ! Sur quel matière","cours",cours),
@@ -77,7 +138,7 @@ async def on_message(message):
         await Help_channel.send(first_node.question)
 
     for child in current_node.list_child_node:
-        if message.content == child.keyword:
+        if child.keyword in message.content :
             await Help_channel.send(child.question)
             current_node = child
 
